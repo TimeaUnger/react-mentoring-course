4371dc2fc01da39aeb66f93d03f7ca86b1e21308
Timea Unger <timea.unger11@gmail.com>
2023-03-20 17:50:27 +0100

    Search form implementation

--- movies-app/src/App.js
+++ movies-app/src/App.js
@@ -1,10 +1,26 @@
-import Counter from './components/Counter';
+import React, { useState} from "react";
 import './App.css';
+import Counter from './components/Counter';
+import SearchForm from './components/SearchForm'
 
 function App() {
+
+  const initialSearch = {
+    searchQuery: 'Star Wars',
+    id: '181808'
+  };
+
+  const [searchQuery, setSearchTerm] = useState(initialSearch);
+
+  const handleSearch = (searchQuery) => {
+    setSearchTerm(searchQuery);
+  }
+
   return (
     <div className="App">
+
       <Counter counterNr={0} />
+      <SearchForm onSearch={handleSearch} searchVal={searchQuery} />
       
     </div>
   );
