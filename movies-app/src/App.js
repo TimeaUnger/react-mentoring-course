cf06ade51bd80cf38e63e0c837887c39a1f98cc4
Timea Unger <timea_unger@epam.com>
2023-03-20 21:45:52 +0100

    genre select implemented

--- movies-app/src/App.js
+++ movies-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState} from "react";
 import './App.css';
-import Counter from './components/Counter';
+// import Counter from './components/Counter';
 import SearchForm from './components/SearchForm'
 import GenreSelect from './components/GenreSelect'
 
@@ -15,15 +15,22 @@ function App() {
 
   const handleSearch = (searchQuery) => {
     setSearchTerm(searchQuery);
+    console.log(searchQuery)
+  }
+
+  const handleGenreSelect = (selectedGenre) => {
+    console.log(selectedGenre)
   }
 
   return (
     <div className="App">
-
-      <Counter counterNr={0} />
-      <SearchForm onSearch={handleSearch} searchVal={searchQuery} />
-      <GenreSelect />
-      
+      <div className="header">
+        {/* <Counter counterNr={0} /> */}
+        <SearchForm onSearch={handleSearch} searchVal={searchQuery} />
+      </div>
+      <div className="pageContent">
+        <GenreSelect onSelect={handleGenreSelect}/>
+      </div>
     </div>
   );
 }
--- movies-app/src/components/GenreSelect.jsx
+++ movies-app/src/components/GenreSelect.jsx
@@ -4,11 +4,13 @@ import './GenreSelect.css';
 const GenreSelect = (props) => {
 
     const [genres, setGenres] = useState([]);
+    const [active, setActive] = useState(false)
+
     useEffect(() => {
       fetch('http://localhost:4000/movies')
          .then((response) => response.json())
          .then((res) => {
-            let genresArr = [];
+            let genresArr = ["All"];
             res.data.forEach(function(value, index, array) {
                 genresArr.push(...value.genres)
             }); 
@@ -21,12 +23,29 @@ const GenreSelect = (props) => {
          });
     }, []);
 
+
+    const onSelectHadler = (event) => {
+      const genre = event.target.innerHTML;
+      props.onSelect(genre) 
+      setActive(genre)
+    }
+
     return (
-      <>
+      <div className="genreSelect">
         <ul>
-          {genres.map((genre, index) =><li key={index}>{ genre }</li>)}
+          {genres.map((genre, index) => {
+            return (
+              <li 
+                key={index} 
+                onClick={onSelectHadler}
+                className={`genreItem ${active === genre && 'active'}`}
+              >
+                { genre }
+              </li>
+            )
+          })}
         </ul>
-      </>
+      </div>
     );
 }
 
