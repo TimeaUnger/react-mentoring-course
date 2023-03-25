a30a5e203fcf676db569b922bd55297c182c5f5c
Timea Unger <timea_unger@epam.com>
2023-03-25 18:31:39 +0100

    Jest setup

---
+++ movies-app/jest.config.js
@@ -0,0 +1,10 @@
+// module.exports = {
+//   "testEnvironment": "jsdom"
+// }
+
+module.exports = {
+  "moduleNameMapper": {
+    "\\.(css|scss)$": "identity-obj-proxy"
+  },
+  "testEnvironment": "jsdom"
+}
