5798017a14098ccc1f23a982b37d45cb25f63606
Timea Unger <timea.unger11@gmail.com>
2023-04-02 17:15:54 +0200

    Refactoring layout

--- movies-app/src/components/GenreSelect.jsx
+++ movies-app/src/components/GenreSelect.jsx
@@ -2,26 +2,6 @@ import React, { useState, useEffect} from "react";
 import './GenreSelect.css';
 
 const GenreSelect = (props) => {
-    
-    // const [genres, setGenres] = useState(props.genres || []);
-    // const [active, setActive] = useState(false)
-
-    // useEffect(() => {
-    //   fetch('http://localhost:4000/movies')
-    //      .then((response) => response.json())
-    //      .then((res) => {
-    //         let genresArr = ["All"];
-    //         res.data.forEach(function(value, index, array) {
-    //             genresArr.push(...value.genres)
-    //         }); 
-
-    //         let genresUnique = genresArr.filter((value, index, array) => array.indexOf(value) === index);
-    //         setGenres(genresUnique)
-    //      })
-    //      .catch((err) => {
-    //         console.log(err.message);
-    //      });
-    // }, []);
 
     const onSelectHandler = (event) => {
       const genre = event.target.innerHTML;
