524476216279b99e14a0d27bbf5ba469c460e126
Timea Unger <timea.unger11@gmail.com>
2023-04-02 17:15:54 +0200

    Refactoring layout

--- movies-app/src/components/SearchForm.jsx
+++ movies-app/src/components/SearchForm.jsx
@@ -1,48 +1,53 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import './SearchForm.css';
 
 const SearchForm = (props) => {
-    
-    const [searchVal, setSearchVal] = useState(props.searchVal);
 
-    const handleSearch = (event) => {
+  const [searchVal, setSearchVal] = useState(props.searchVal);
 
-        if (event.keyCode === 13){
-            props.onSearch(searchVal);
-        }
+  const handleSearch = (event) => {
 
-        if(event.type === "focus"){
-            props.onSearch(searchVal);
-        }
-    };
-
-    const handleBtnClick = () => { 
-        props.onSearch(searchVal);
-    }
-    
-    const inputHandler = (event) => {
-        
-        setSearchVal(event.target.value)
+    if (event.keyCode === 13) {
+      props.onSearch(searchVal);
     }
 
-    return (
-        <div className="searchWrapper">
-            <div className="searchInput">
-                <input
-                    type="search"
-                    data-testid="searchInput"
-                    placeholder="Search movie"
-                    onKeyDown={handleSearch}
-                    onFocus={handleSearch}
-                    onChange={inputHandler}
-                    value={searchVal}
-                />
-            </div>
-           <div className="searchButton">
-                <button data-testid="searchButton" onClick={handleBtnClick} >Search</button>
-           </div>
+    if (event.type === "focus") {
+      props.onSearch(searchVal);
+    }
+  };
+
+  const handleBtnClick = () => {
+    props.onSearch(searchVal);
+  }
+
+  const inputHandler = (event) => {
+
+    setSearchVal(event.target.value)
+  }
+
+  return (
+    <div className="searchWrapper">
+      <div className="searchInnerContent">
+        <div className="findMovieLabel">Find your movie</div>
+        <div className="searchInputRow">
+          <div className="searchInput">
+            <input
+              type="search"
+              data-testid="searchInput"
+              placeholder="Search movie"
+              onKeyDown={handleSearch}
+              onFocus={handleSearch}
+              onChange={inputHandler}
+              value={searchVal}
+            />
+          </div>
+          <div className="searchButton">
+            <button data-testid="searchButton" onClick={handleBtnClick} >Search</button>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 
 }
 export default SearchForm;
